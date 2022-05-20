775548cd5630737d4372222b4ea92928672ea5ef
Tom Zaspel <t.zaspel@finatix.de>
2022-05-20 20:39:16 +0200

    rename setup shell script

---
+++ install-prerequisites.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Update System"
+sudo dnf -y update
+
+echo "installing pyhton3"
+sudo dnf install python3
+
+echo "installing Pip"
+sudo dnf install python3-pip
+
+echo "installing Ansible"
+pip install ansible
+
+echo "installing Ansible community modules"
+ansible-galaxy collection install community.general
