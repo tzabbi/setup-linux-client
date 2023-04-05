494a9b6aaadaf5aecb9863d9a4532cf52729192b
Tom Zaspel <tom@zaspel.it>
2023-04-05 19:36:49 +0200

    add  support for debian based distros

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
 echo "Update System"
-sudo dnf -y update
-
-echo "installing pyhton3"
-sudo dnf install python3
-
-echo "installing Pip"
-sudo dnf install python3-pip
+ case $distro in
+    fedora)
+        echo "Distribution: Fedora"
+        sudo dnf -y update
+        echo "installing pyhton3"
+        sudo dnf install python3
+        echo "installing Pip"
+        sudo dnf install python3-pip
+    ;;
+    
+    ubuntu | pop)
+        sudo apt update 
+        sudo apt install -y software-properties-common wget build-essential libncursesw5-dev libssl-dev \
+          libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+        sudo add-apt-repository ppa:deadsnakes/ppa
+        sudo apt install python3.11
+        sudo apt install python3-pip
+    ;;
 
 echo "installing Ansible"
 pip install ansible
