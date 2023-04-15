819a8037de174428324b2e5c240c1dad398a3a8d
Tom Zaspel <tom@zaspel.it>
2023-04-15 13:20:38 +0200

    fix ubuntu issue

--- install-prerequisites.sh
+++ install-prerequisites.sh
@@ -7,7 +7,7 @@ case $distro in
         echo "Distribution: Fedora"
         sudo dnf -y update
         echo "installing pyhton3"
-        sudo dnf install python3
+        sudo dnf install python3 -y
         echo "installing Pip"
         sudo dnf install python3-pip
     ;;
@@ -15,13 +15,16 @@ case $distro in
     ubuntu | pop)
         echo "Distribution: $distro"
         sudo apt update
-        sudo apt install -y software-properties-common wget build-essential libncursesw5-dev libssl-dev \
-          libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+        sudo apt install software-properties-common wget build-essential libncursesw5-dev libssl-dev \
+          libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
         echo "installing pyhton3"
         sudo add-apt-repository ppa:deadsnakes/ppa
-        sudo apt install python3.11
+        sudo apt install python3.11 -y
         echo "installing Pip"
-        sudo apt install python3-pip
+        sudo apt install python3-pip -y
+        echo "reboot your system in 10 seconds. To stop this process pls press Ctrl + C"
+        sleep 10
+        reboot now
     ;;
     *)
       echo "Distro: $distro is currently not supported. Pls create an Issue or a Pull Request"
@@ -36,4 +39,3 @@ pip install ansible
 
 echo "installing Ansible community modules"
 ansible-galaxy collection install community.general
-
