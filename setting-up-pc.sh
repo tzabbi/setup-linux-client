cee31568b5d6dc4ca32237c1848104d5079b6782
$full_name <$email>
2023-11-03 18:34:08 +0100

    using apt to install ansible for debian based linux distros

--- setting-up-pc.sh
+++ setting-up-pc.sh
@@ -15,16 +15,7 @@ case $distro in
     ubuntu | pop)
         echo "Distribution: $distro"
         sudo apt update
-        sudo apt install software-properties-common wget build-essential libncursesw5-dev libssl-dev \
-          libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev -y
-        echo "installing pyhton3"
-        sudo add-apt-repository ppa:deadsnakes/ppa
-        sudo apt install python3.11 -y
-        echo "installing Pip"
-        sudo apt install python3-pip -y
-        echo "reboot your system in 10 seconds. To stop this process pls press Ctrl + C"
-        sleep 10
-        reboot now
+        sudo apt install ansible
     ;;
     *)
       echo "Distro: $distro is currently not supported. Pls create an Issue or a Pull Request"
